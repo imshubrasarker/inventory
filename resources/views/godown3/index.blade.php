6fa1af8f665afbfc581b2f6e83b49086982ef6b0
Kamrul Islam Tushar <ki.tushar21@gmail.com>
2019-12-21 10:36:13 +0600

    Search issue

--- resources/views/godown3/index.blade.php
+++ resources/views/godown3/index.blade.php
@@ -17,7 +17,7 @@
                     <div class="form-body">
                         <div class="card">
                             <div class="card-body">
-                                {!! Form::open(['method' => 'GET', 'url' => '/stocks', 'role' => 'search'])  !!}
+                                {!! Form::open(['method' => 'GET', 'url' => '/godown-3', 'role' => 'search'])  !!}
                                 <div class="row">
                                     <div class="col-md-4">
                                         <select class="form-control" name="product_id" id="product_id">
